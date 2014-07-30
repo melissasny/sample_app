a059b5a16ec5a849719f3e24db1a91508044638d
Melissa Shields <mshieldsny@gmail.com>
2014-07-30 13:42:25 -0400

    Finish layout and routes

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # Make bootstrap-sass compatible with the asset pipeline
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
